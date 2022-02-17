ac65ddd2833758ffe5ea268dc766d5ce33687aff
Katonic-ML-Marketplace <98009207+Katonic-ML-Marketplace@users.noreply.github.com>
2022-02-17 20:06:32 +0530

    App Added

--- app/build.sh
+++
@@ -1,3 +0,0 @@
-docker build -t katonic/apps:predictive-maintenance-intel .
-docker push katonic/apps:predictive-maintenance-intel
-# docker run --rm -p 8050:8050 katonic/apps:predictive-maintenance-intel
